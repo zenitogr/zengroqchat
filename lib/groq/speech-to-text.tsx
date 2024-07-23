05f472792c67f1cd1bb9defe486a9cd52de67c64
ZenitoGR <zengodgr@outlook.com>
2024-07-23 15:05:45 +0300

    test

--- lib/groq/speech-to-text.tsx
+++ lib/groq/speech-to-text.tsx
@@ -1,15 +1,6 @@
 "use server"
 import Groq from "groq-sdk";
-import { Readable } from "stream";
-import fs from 'fs';
-import { blobToBase64 } from '@/utils/blobToBase64';
-function createStreamFromBase64(base64data: string): Readable {
-  const buffer = Buffer.from(base64data, "base64");
-  const stream = new Readable();
-  stream.push(buffer);
-  stream.push(null); // Signals the end of the stream
-  return stream;
-}
+
 export async function getGroqSpeechToText(audioBlob:Blob) {
     const groq = new Groq({apiKey: process.env.GROQ_API_KEY});
     //create Filelike from audioBlob
